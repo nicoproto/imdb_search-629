4946af5ca5a65ef908b2f4c90383fcc2b009437b
Nico Proto <nicolasproto@gmail.com>
2021-06-10 09:59:42 +0200

    Finished lecture

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,20 @@
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      # sql_query = " \
+      #   movies.title @@ :query \
+      #   OR movies.synopsis @@ :query \
+      #   OR directors.first_name @@ :query \
+      #   OR directors.last_name @@ :query \
+      # "
+
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+
+      # @movies = Movie.global_search(params[:query])
+
+      @movies = PgSearch.multisearch(params[:query]).map { |result| result.searchable }
+    else
+      @movies = Movie.all
+    end
+  end
+end
